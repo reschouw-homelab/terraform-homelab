521d0b10d9f2a40fd05b579f518267a2d02b8392
dorwin <dorwin@dorwinia.com>
2020-05-02 17:50:37 -0700

    Cleaning up files, and unsetting maintenence mode

--- hosts/host.tf
+++ hosts/host.tf
@@ -1,9 +1,8 @@
 resource "vsphere_host" "esxi-1" {
   hostname = "esxi-1"
-  #datacenter = data.vsphere_datacenter.dc.id
   username = ""
   password = ""  
   connected = true
-  maintenance = true
+  maintenance = false
   lockdown = "disabled"
 }
