7c8e581d6ea427da18662e846bfb0c0106ea21a6
dorwin <dorwin@dorwinia.com>
2020-08-07 13:27:57 -0700

    first attempt at using secrets service to handle sensitive creds

--- vsphere/ansible-test/provider.tf
+++ vsphere/ansible-test/provider.tf
@@ -1,8 +1,13 @@
 provider "vsphere" {
-  user           = "***REMOVED***"
-  password       = "***REMOVED***"
+  user           = local.vcenter-creds.username
+  password       = local.vcenter-creds.password
   vsphere_server = "vcenter.dorwinia.com"
   version = "~> 1.18.3"
   # If you have a self-signed cert
   allow_unverified_ssl = true
+}
+
+provider "aws" {
+  region = "us-west-2"
+  version = "~> 3.0"
 }
