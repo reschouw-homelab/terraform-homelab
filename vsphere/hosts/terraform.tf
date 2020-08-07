933ef69ae0b6e1ecba70313c3e38cb3ac5c4dd02
dorwin <dorwin@dorwinia.com>
2020-08-07 15:18:24 -0700

    removing some leftover cruft from testing

--- vsphere/hosts/terraform.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "dorwinia-tf-state"
-    key    = "dorwinia-tf-state/hosts.tfstate"
-    region = "us-west-2"
-  }
-}
--- vsphere/module-test/terraform.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "dorwinia-tf-state"
-    key    = "terraform-homelab/module-test.tfstate"
-    region = "us-west-2"
-  }
-}
