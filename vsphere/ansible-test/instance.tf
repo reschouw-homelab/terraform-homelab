15de5745f3029b23c8f62e51e89db899a8f66612
dorwin <dorwin@dorwinia.com>
2020-08-13 16:13:38 -0700

    messing around with terraform 0.13

--- vsphere/ansible-test/instance.tf
+++ vsphere/ansible-test/instance.tf
@@ -1,27 +1,28 @@
-module "ans-1" {
+module "test-instances" {
+  count = 2
   source = "../modules/vsphere-instance"
 
-  hostname = "ans-1"
+  hostname = "ans-${count.index+1}"
   playbook = "common.yml"
-  ip-address = "192.168.1.51/24"
+  ip-address = "192.168.1.5${count.index+1}/24"
   gateway = "192.168.1.1"
   dns-servers = "192.168.1.11, 192.168.1.12"
   num-cpus = 2
   memory = 2048
   disk-size = 8
-  template = "ubuntu-18-04-20200709140801"
+  template = "ubuntu-18-04-20200810233459"
 }
 
-module "ans-2" {
-  source = "../modules/vsphere-instance"
-
-  hostname = "ans-2"
-  playbook = "common.yml"
-  ip-address = "192.168.1.52/24"
-  gateway = "192.168.1.1"
-  dns-servers = "192.168.1.11, 192.168.1.12"
-  num-cpus = 2
-  memory = 2048
-  disk-size = 8
-  template = "ubuntu-18-04-20200709140801"
-}
+#module "ans-2" {
+#  source = "../modules/vsphere-instance"
+#
+#  hostname = "ans-2"
+#  playbook = "common.yml"
+#  ip-address = "192.168.1.52/24"
+#  gateway = "192.168.1.1"
+#  dns-servers = "192.168.1.11, 192.168.1.12"
+#  num-cpus = 2
+#  memory = 2048
+#  disk-size = 8
+#  template = "ubuntu-18-04-20200709140801"
+#}
--- vsphere/ansible-test/terraform.tf
+++ vsphere/ansible-test/terraform.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 0.13" 
   backend "s3" {
     bucket = "dorwinia-tf-state"
     key    = "terraform-homelab/ansible-test.tfstate"
@@ -7,6 +8,7 @@ terraform {
 }
 
 provider "vsphere" {
+  #source = "hashicorp/vsphere"
   user           = local.vcenter-creds.username
   password       = local.vcenter-creds.password
   vsphere_server = "vcenter.dorwinia.com"
@@ -16,6 +18,7 @@ provider "vsphere" {
 }
 
 provider "aws" {
+  #source = "hashicorp/aws"
   region = "us-west-2"
   version = "~> 3.0"
 }
