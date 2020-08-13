8f13262dcb8d80c8f445607fcf292a56e05136b9
dorwin <dorwin@dorwinia.com>
2020-08-12 17:02:14 -0700

    upsizing graylog

--- vsphere/graylog/instance.tf
+++ vsphere/graylog/instance.tf
@@ -6,8 +6,8 @@ module "graylog" {
   ip-address = "192.168.1.148/24"
   gateway = "192.168.1.1"
   dns-servers = "192.168.1.11, 192.168.1.12"
-  num-cpus = 2
-  memory = 2048
+  num-cpus = 4
+  memory = 8096
   disk-size = 16
   template = "ubuntu-18-04-20200810233459"
 }
