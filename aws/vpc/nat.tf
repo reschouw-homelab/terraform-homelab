1f568ac568be5ca965d1ec5b7404fcba174d9f8b
dorwin <dorwin@dorwinia.com>
2021-05-02 04:20:23 +0000

    moving nat instance into dmz

--- aws/vpc/nat.tf
+++ aws/vpc/nat.tf
@@ -2,10 +2,10 @@ module "nat" {
   source = "git@github.com:reschouw/terraform-homelab.git//aws/modules/aws-instance?ref=aws-instance-v2.2"
   
   hostname = "nat"
-  subnet = "us-west-2a"
+  subnet = "us-west-2-dmz"
   playbook = "nat.yml"
   associate-public-ip = true
-  ip-address = "10.1.0.10"
+  ip-address = "10.1.4.10"
   
   instance-type = "t3a.nano"
 }
