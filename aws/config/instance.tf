eae48337fd0e8aa4c1955595f98be684c2bb95b4
Ryan Schouweiler <reschouw@gmail.com>
2021-06-21 16:31:49 -0700

    increasing config disk size

--- aws/config/instance.tf
+++ aws/config/instance.tf
@@ -1,10 +1,11 @@
 module "config" {
-  source = "git@github.com:reschouw/terraform-homelab.git//aws/modules/aws-instance?ref=aws-instance-v3.1"
+  source = "git@github.com:reschouw/terraform-homelab.git//aws/modules/aws-instance?ref=aws-instance-v3.4"
   #source = "/home/dorwin/terraform-homelab/aws/modules/aws-instance"
   
   hostname = "config"
   ami-name = "config-pre-ebs-expansion"
   instance-type = "t3a.small"
+  disk-size = 12
   subnet = "us-west-2-dmz"
   associate-public-ip = true
   custom-security-group-ids = [aws_security_group.config.id]
