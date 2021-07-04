313f6d120f03d617e99a24a0a547921eeb333c97
dorwin <dorwin@dorwinia.com>
2021-07-04 16:14:03 +0000

    updating module version

--- aws/k8s/controller.tf
+++ aws/k8s/controller.tf
@@ -1,6 +1,6 @@
 module "controller" {
-  #source = "git@github.com:reschouw/terraform-homelab.git//aws/modules/aws-instance?ref=aws-instance-v3.5"
-  source = "/home/dorwin/terraform-homelab/aws/modules/aws-instance"
+  source = "git@github.com:reschouw/terraform-homelab.git//aws/modules/aws-instance?ref=aws-instance-v3.7"
+  #source = "/home/dorwin/terraform-homelab/aws/modules/aws-instance"
   
   hostname = "kube-con-1"
   subnet = "us-west-2a"
