62fc8be4bb93cea22618ec5a7c21a8c34a8dbe64
dorwin <dorwin@dorwinia.com>
2021-07-04 15:52:09 +0000

    adding private ip to module outputs

--- aws/modules/aws-instance/outputs.tf
+++ aws/modules/aws-instance/outputs.tf
@@ -3,6 +3,11 @@ output "public-ip" {
   value = var.spot-instance ? aws_spot_instance_request.instance[0].public_ip : aws_instance.instance[0].public_ip
 }
 
+output "private-ip" {
+  description = "The public IP address of the created instance"
+  value = var.spot-instance ? aws_spot_instance_request.instance[0].private_ip : aws_instance.instance[0].private_ip
+}
+
 output "instance-id" {
   description = "The id of the created instance"
   value = var.spot-instance ? aws_spot_instance_request.instance[0].spot_instance_id : aws_instance.instance[0].id
