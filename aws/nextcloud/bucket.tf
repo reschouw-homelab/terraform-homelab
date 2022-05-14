1388e90575eddbd992b1b5e2001ad29da9f55e71
Ryan Schouweiler <reschouw@gmail.com>
2022-05-14 15:53:10 +0000

    Adding bucket versioning
    [Ticket: LAB-25]

--- aws/nextcloud/bucket.tf
+++ aws/nextcloud/bucket.tf
@@ -6,3 +6,26 @@ resource "aws_s3_bucket_acl" "nextcloud" {
   bucket = aws_s3_bucket.nextcloud.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_versioning" "nextcloud" {
+  bucket = aws_s3_bucket.nextcloud.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "nextcloud" {
+  bucket = aws_s3_bucket.nextcloud.id
+  depends_on = [aws_s3_bucket_versioning.nextcloud]
+  rule {
+    id = "version-expire"
+    status = "Enabled"
+    noncurrent_version_expiration {
+      noncurrent_days = 60
+    }
+    noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class = "ONEZONE_IA"
+    }
+  }
+}
