98b838048e642329afa1432366e6dabe447c3764
Ryan Schouweiler <reschouw@gmail.com>
2022-05-14 15:16:04 +0000

    Updating to aws provider v4.14

--- aws/nextcloud/bucket.tf
+++ aws/nextcloud/bucket.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "nextcloud" {
   bucket = "dorwinia-nextcloud"
+}
+
+resource "aws_s3_bucket_acl" "nextcloud" {
+  bucket = aws_s3_bucket.nextcloud.id
   acl    = "private"
 }
