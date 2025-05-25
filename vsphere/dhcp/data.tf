9231096ee37c8349de153f8f258ae92fe01f2c71
Ryan Schouweiler <reschouw@gmail.com>
2025-05-25 13:59:11 -0700

    deleting vsphere config

--- vsphere/dhcp/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_secretsmanager_secret_version" "vcenter-creds" {
-  secret_id = "vcenter-creds"
-}
-
-locals {
-  vcenter-creds = jsondecode(
-    data.aws_secretsmanager_secret_version.vcenter-creds.secret_string
-  )
-}
