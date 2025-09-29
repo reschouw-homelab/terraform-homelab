6aae9c1f05e2a3c7249272a866cd381b99e48704
Ryan Schouweiler <reschouw@gmail.com>
2025-09-29 15:01:50 -0700

    removing ingress-nginx until I can be troubled to figure out metallb

--- kube-bootstrap/ingress-nginx.tf
+++ kube-bootstrap/ingress-nginx.tf
@@ -1,11 +1,14 @@
-resource "helm_release" "ingress_nginx" {
-    name = "ingress-nginx"
-    repository = "https://kubernetes.github.io/ingress-nginx"
-    chart = "ingress-nginx"
-    namespace = "ingress-nginx"
-    version = "4.13.2"
-    create_namespace = true
-    # I think the terraform provider is expecting something more than the 
-    # controller to exist, and this helm chart doesn't do that? 
-    wait = false
-}
+# resource "helm_release" "ingress_nginx" {
+#     name = "ingress-nginx"
+#     repository = "https://kubernetes.github.io/ingress-nginx"
+#     chart = "ingress-nginx"
+#     namespace = "ingress-nginx"
+#     version = "4.13.2"
+#     create_namespace = true
+#     # I think the terraform provider is expecting something more than the 
+#     # controller to exist, and this helm chart doesn't do that? 
+#     wait = false
+#     values = [
+#         file("ingress-nginx/values.yaml")
+#     ]
+# }
